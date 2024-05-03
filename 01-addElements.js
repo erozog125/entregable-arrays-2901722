fd95d48c32f2eebade6ac80460e9e8a026778a7d
sofi11108 <sofia35025@gmail.com>
2024-05-03 16:11:48 -0500

    rama nicolas giraldo

---
+++ 01-addElements.js
@@ -0,0 +1,7 @@
+// Suma de elementos: Escribe una función que tome un array de números como parámetro y devuelva la suma de todos los elementos.
+
+const numbers = [12,3,4,5,66,77,-12,78,0,5]
+
+function addNumbers (array) {
+  return sum
+}
