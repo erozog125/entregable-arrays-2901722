cc7a5bec7193f1611b01d82e33c0854b381a7c3d
nicolas <ortiznicolas656@gmail.com>
2024-05-03 17:44:27 -0500

    feat: Add new excercise about array numbers.Miguel_Chilito

--- 03_delete duplicate.js
+++ 03_delete duplicate.js
@@ -1,3 +1,14 @@
-function deleteDuplicate(array) {
-    
-}
+function deleteDuplicates(array) {
+    // Utilizamos un conjunto para almacenar elementos únicos
+    const uniqueElements = array.filtrer(value, index, );
+    // Convertimos el conjunto de nuevo a un array
+    const uniqueArray = [...uniqueElements];
+    return uniqueArray;
+  }
+  
+  const numbersWithDuplicates = [11, 222, 222, 45, 11, 45, 7, 100];
+  const uniqueNumbers = deleteDuplicates(numbersWithDuplicates);
+  
+  console.log("Array original:", numbersWithDuplicates);
+  console.log("Array sin duplicados:", uniqueNumbers);
+  
