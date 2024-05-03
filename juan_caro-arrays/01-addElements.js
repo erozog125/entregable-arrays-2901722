ec9c520c20bd07cad17c55335f1b448c43560c4a
juan-david <carovargasjuandavid494@gmail.com>
2024-05-03 17:39:28 -0500

    save exercise 1

--- juan_caro-arrays/01-addElements.js
+++ juan_caro-arrays/01-addElements.js
@@ -2,6 +2,8 @@
 
 const numbers = [12,3,4,5,66,77,-12,78,0,5]
 
-function addNumbers (array) {
-  return sum
+function addNumbers (numbers) {
+  return numbers.reduce((a,b)=> a+b)
+
 }
+console.log(addNumbers(numbers));
