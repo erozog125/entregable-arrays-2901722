c509b9a6ce04a1e013429f3d59e1a47b94b5d716
juban1234 <alexzo8677gmail.com>
2024-05-03 12:53:47 -0500

    feat: Add new excercise about adding array numbers.juan_esteban_grajales feat(jg):

--- 01-addElements.js
+++ 01-addElements.js
@@ -2,6 +2,17 @@
 
 const numbers = [12,3,4,5,66,77,-12,78,0,5]
 
-function addNumbers (array) {
-  return sum
+function addNumbers(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
 }
+
+function Start() {
+  let sum = addNumbers(numbers);
+  console.log(sum);
+}
+
+Start();
