4bab1700c63845b434019743285624826ac9cd4a
DavidStevenToro <davidtoro013125@gmail.com>
2024-05-03 17:22:19 -0500

    feat: Add new excercise activity_1 sum of numbers.David_Steven_Toro

---
+++ David_Steven_Toro_Bejarano-Arrays/activity1_sumOfElements.js
@@ -0,0 +1,16 @@
+let numbers = [1, 2, 5]
+
+function addElements(numbers) {
+    let counter = 0
+    numbers.forEach(number => {
+        counter += number
+    });
+    return counter
+}
+
+console.log(addElements(numbers))
+
+
+
+
+
