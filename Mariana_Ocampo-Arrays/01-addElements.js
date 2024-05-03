3208575a18ae5ee4d19a667314de7315ad6a2154
unknown <SenaTarde@ARMAPRCCIFOD600.FormacionSena.RED>
2024-05-03 17:45:31 -0500

    excercise 1

---
+++ Mariana_Ocampo-Arrays/01-addElements.js
@@ -0,0 +1,9 @@
+const numbers = [12,3,4,5,66,77,-12,78,0,5]
+
+function addNumbers (array) {
+ const sum = array.reduce((result, element) => result + element, 0);
+ console.log("La suma de los elementos es: ", sum);
+ 
+  return sum
+}
+addNumbers(numbers)
