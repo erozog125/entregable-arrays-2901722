5b1374f47e18eacb2d610e606c8802fa98ac59ec
CamiloDuranMesa <168236046+CamiloDuranMesa@users.noreply.github.com>
2024-05-03 17:31:07 -0500

    Update class01-arrays.js

--- class01-arrays.js
+++ class01-arrays.js
@@ -5,6 +5,6 @@ let names = ["Juan", "María", "Pedro", "Ana", "Luis", "Sofía", "Carlos", "Laur
 let fruits = ["manzana", "pera", "banana", "naranja", "uva", "sandía", "kiwi", "mango", "piña", "cereza"];
 
 // Array de números
-// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
