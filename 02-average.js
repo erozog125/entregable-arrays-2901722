8c5263732eb60c1a32b8f38b7a2ee6765937f960
unknown <SenaTarde@ARMAPRCCIFOD593.FormacionSena.RED>
2024-05-03 17:10:48 -0500

    feat: promedio de numeros .Santiago_Aguirre

---
+++ 02-average.js
@@ -0,0 +1,12 @@
+let numbers=[20,30,20,10]
+function addition(n1,n2){
+    return n1+n2
+}
+
+let average=numbers.reduce(addition)
+
+function divison(b){
+   return b/numbers.length
+}
+
+console.log(divison(average))
