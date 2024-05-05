0b2047bbea18c514d9105495ce11b51c67650e4c
Daniel bañol <daniel.banol58@gmail.com>
2024-05-04 22:15:52 -0500

    feat: save exercise 3.Daniel_bañol

---
+++ Jose_daniel_bañol-Arrays/03-removeDuplicateNumbers.js
@@ -0,0 +1,14 @@
+const numbers = [2, 2, 3, 4, 10, 6, 7, 3, 9, 10];
+
+function removeDuplicateNumbers() {
+    let deleteNumbers = new Set(numbers)
+
+    let newArray = [...deleteNumbers]
+
+    return newArray
+
+} 
+
+  
+
+  
