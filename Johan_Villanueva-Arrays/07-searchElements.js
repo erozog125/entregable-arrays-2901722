e1d54842411f892a8ac3bac21f9ea9fa118b6561
Johan Villanueva <jvillanuevagallego@gmail.com>
2024-05-05 18:20:22 -0500

    feat: Add new excercise about adding array 07-searchElements

---
+++ Johan_Villanueva-Arrays/07-searchElements.js
@@ -0,0 +1,7 @@
+const numberOne = [2,3,4,1]
+const numberTwo = 5
+function findNumber(numberOne,numberTwo) {
+    return numberOne.includes(numberTwo)
+}
+console.log(findNumber(numberOne,numberTwo))
+
