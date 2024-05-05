544ebf4616fb54d457596ddbdac0fbb6341726e0
Daniel bañol <daniel.banol58@gmail.com>
2024-05-04 20:43:24 -0500

    save exercise 2.Daniel_bañol

---
+++ Jose_daniel_bañol-Arrays/02-averageNumbers.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function averageNumbers() {
+  let average =  numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0) /numbers.length;
+  return average
+  
+} 
+  
+
