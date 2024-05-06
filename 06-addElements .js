816ff9296f2d8e3860f107ea48bde0b33d8461ae
juban1234 <alexzo8677gmail.com>
2024-05-06 14:29:13 -0500

    error correction

--- 06-addElements .js
+++ 06-addElements .js
@@ -1,9 +1,5 @@
 function ordenarNumeros(array) {
     let sortedArray = array.slice().sort((a, b) => a - b);
     return sortedArray;
-  }
-  
-  const numeros = [12, 3, 4, 5, 66, 77, -12, 78, 0, 5];
-  const numerosOrdenados = ordenarNumeros(numeros);
-  console.log(numerosOrdenados);
+}
   
