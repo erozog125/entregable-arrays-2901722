17c4e741b592f8393aa8c8517bb12cd092fded85
David_Muñoz <davidmunozmoreno2@gmail.com>
2024-05-06 10:05:37 -0500

    se elimino un archivo

--- David_Muñoz_Moreno-arrays/07-investArray.js
+++
@@ -1,8 +0,0 @@
-nambers=["faber","godoy","superboy","william"]
-function invest(nambers){
-   let newNambers= nambers.slice(0,nambers.length)
-   newNambers.reverse()
-    return newNambers
-}
-let exit = invest(nambers)
-console.log(exit)
