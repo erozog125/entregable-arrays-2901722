ad94e8ee8cd4e6200fbf89a3af1867a41630a45e
katalina <katalina4576@gmail.com>
2024-05-07 13:41:54 -0500

    function that takes an array of numbers as an argument and returns an object with two properties: one that contains the number of even numbers and another that contains the number of odd numbers in the array.Katalina Ortiz

---
+++ Katalina_Ortiz-arrays/06-evenandodd.js
@@ -0,0 +1,26 @@
+// Definimos la función que cuenta la cantidad de números pares e impares en un array
+function countEvenOdd(array) {
+    // Inicializamos variables para contar los números pares e impares
+    let quantityPairs = 0;
+    let quantityOdd = 0;
+
+    // Iteramos sobre cada elemento del array
+    for (let i = 0; i < array.length; i++) {
+        // Verificamos si el número en la posición actual es par o impar
+        if (array[i] % 2 === 0) {
+            // Si es par, incrementamos la cantidad de pares
+            quantityPairs++;
+        } else {
+            // Si es impar, incrementamos la cantidad de impares
+            quantityOdd++;
+        }
+    }
+
+    // Creamos un objeto con las propiedades cantidadPares y cantidadImpares y las devolvemos
+    const result = {
+        quantityPairs: quantityPairs,
+        quantityOdd: quantityOdd
+    };
+
+    return result;
+}
