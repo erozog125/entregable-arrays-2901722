b85b0b7ee2a2b4d82a2a6edf7325f52f5e9cc7e6
katalina <katalina4576@gmail.com>
2024-05-07 13:46:31 -0500

    function that takes an array as an argument and returns a new array with the elements in reverse order.Katalina Ortiz

---
+++ Katalina_Ortiz-arrays/07-Invest.js
@@ -0,0 +1,8 @@
+// Definimos la función que devuelve un nuevo array con los elementos en orden inverso
+function invertArray(array) {
+    // Utilizamos el método reverse para invertir el orden del array
+    const arrayInverted = array.reverse();
+    
+    // Devolvemos el array invertido
+    return arrayInverted;
+}
