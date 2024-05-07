c3bdb99dc5e7d52621489f1b779cecad88dc9e57
karinatabares1 <marianaocampomedina@gmail.com>
2024-05-07 18:28:54 -0500

    feat: invert Array. Mariana_Ocampo

---
+++ Mariana_Ocampo-Arrays/07-invertArray.js
@@ -0,0 +1,7 @@
+function invertArray(array) {
+    return array.reverse();
+}
+
+const originalArray = [1, 2, 3, 4, 5];
+const invertArrayArray = invertArray(originalArray);
+console.log(invertArrayArray); 
