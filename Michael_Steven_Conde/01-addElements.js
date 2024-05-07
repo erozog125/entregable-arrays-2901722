ad717bc04f2343dc9aa6adbf3fce3836b8468a56
unknown <SenaTarde@ARMAPRRQDFSD093.FormacionSena.RED>
2024-05-07 13:19:59 -0500

    feat: Add new excercise 04-addElements.js.Michael_Steven_Conde

--- Michael_Steven_Conde/01-addElements.js
+++ Michael_Steven_Conde/01-addElements.js
@@ -2,4 +2,4 @@ const number=[1,2,3,4,5,6,7,8,9]
 function addNumber(n1,n2){
     return n1+n2
 }
-console.log(number.reduce(addNumber))cd
+console.log(number.reduce(addNumber))
---
+++ Michael_Steven_Conde/04-addElements.js
@@ -0,0 +1,5 @@
+const numbers= [10,2,45,4,5,6,7,8,]
+function lowerNumber(numbers){
+    return Math.min(...numbers)
+}
+console.log(lowerNumber(numbers))
