a1a4b769c338fe3ca852a3c5813508550d22e9fb
juban1234 <alexzo8677gmail.com>
2024-05-07 13:31:29 -0500

    feat: Addin excercise number 9.JE

--- 09-addElements.js
+++ 09-addElements.js
@@ -4,7 +4,5 @@ function SumOfTwoArrays(numbers1,numbers2) {
     const totalSum = sum1 + sum2;
     return totalSum;
 }
- const numbers1 = [1, 2, 3, 4, 5];
- const numbers2 = [6, 7, 8, 9, 10];
- console.log(SumOfTwoArrays(numbers1, numbers2));
+
 
