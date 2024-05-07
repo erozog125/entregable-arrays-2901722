e2f9f25e999181e535f9529bee4be9c85c4fd0a2
MateoHenao06 <henaom001@gmail.com>
2024-05-06 20:01:55 -0500

    feat: Changing Cycle to Array Method.Mateo_Henao

--- Mateo_Henao-Arrays/02-averageItems.js
+++ Mateo_Henao-Arrays/02-averageItems.js
@@ -1,12 +1,13 @@
 const numbers = [10, 20, 30, 40, 50];
-function calcularPromedio(array) {
+function calculateAverage(array) {
     if (array.length === 0) {
-        return "El array está vacío. No se puede calcular el promedio.";
+        console.log("El array está vacío. No se puede calcular el promedio.");
+        return;
     }
 
-    const sum = array.reduce((acumulator, elemet) => acumulator + elemet, 0);
+    const sum = array.reduce((acumulator, element) => acumulator + element, 0);
     const average = sum / array.length;
 
-    return  console.log("El promedio de los elementos es:", average);
+    console.log("El promedio de los elementos es:" +average);
 }
-calcularPromedio(numbers);
+calculateAverage(numbers);
