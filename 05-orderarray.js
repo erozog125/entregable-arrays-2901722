08a852374ac68af55f05d038ad3b3f97173ff019
osorrio@gmail.com <osorio@gmail.com>
2024-05-07 13:48:02 -0500

    feat: order arrays, brayanaguirre

--- 05-orderarray.js
+++ 05-orderarray.js
@@ -1,6 +1,6 @@
 function sortlist() {
-    let messy=[2,3,7,8,0,1,9,4,5,6];
-    console.table(messy.sort())
+    let messy=[2,3,7,8,0,1,9,4,5,6,20];
+    console.table(messy.sort((a,b) => a - b))
 }
 
 sortlist()
