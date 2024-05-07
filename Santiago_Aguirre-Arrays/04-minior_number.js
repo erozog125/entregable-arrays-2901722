6537bf2b5aaa37ee1727d720da2c02ad9f6dfeb9
unknown <SenaTarde@ARMAPRCCIFOD593.FormacionSena.RED>
2024-05-07 13:34:58 -0500

    feat:Imprimir el numero menor de una lista ordenada

--- Santiago_Aguirre-Arrays/04-minior_number.js
+++ Santiago_Aguirre-Arrays/04-minior_number.js
@@ -1,6 +1,7 @@
+let numbers = [2,6,7,1,8,22,4]
+
 function minior(){
-    let numbers = [2,6,7,1,8,22,4]
     let minior_numbers = numbers.sort((a, b) => a - b).shift()
     return minior_numbers
 }
-console.log(minior())
+console.log(minior(numbers))
