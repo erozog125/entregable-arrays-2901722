d65c44b7a4b365db7abacba849a02eee61851968
Nico Ortiz <ortiznicolas656@gmail.com>
2024-05-07 15:29:55 -0500

    update exercise 3 -nicolas ortiz

---
+++ nicolas_ortiz-arrays/03removeDuplicateNumbers.js
@@ -0,0 +1,12 @@
+const numbers = [2, 2, 3, 4, 10, 6, 7, 3, 9, 10];
+
+function removeDuplicateNumbers() {
+    let deleteNumbers = new Set(numbers)
+
+    let newArray = [...deleteNumbers]
+
+    return newArray
+
+} 
+
+  
