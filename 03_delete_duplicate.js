2ef3e427213697288d3bdc1e6cf47f14473baaec
nicolas <ortiznicolas656@gmail.com>
2024-05-07 13:20:56 -0500

    feat: Add new excercise about delete duplicate numbres in array numbers.Miguel_Chilito

---
+++ 03_delete_duplicate.js
@@ -0,0 +1,8 @@
+function deleteDuplicates(array) {
+    const uniqueElements = array.filtrer((value, index, self )=> {
+     uniqueArray =self.indexOF (value) == index;
+    })
+    return uniqueArray;
+  }
+
+  
