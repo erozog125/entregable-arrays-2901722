3391d33f9d5ef7760137fbc6048ee14a401a243f
MateoHenao06 <henaom001@gmail.com>
2024-05-06 19:53:18 -0500

    feat: Switching from Spanish to English.Mateo_Henao

--- Mateo_Henao-Arrays/02-averageItems.js
+++ Mateo_Henao-Arrays/02-averageItems.js
@@ -1,12 +1,12 @@
-const numeros = [10, 20, 30, 40, 50];
+const numbers = [10, 20, 30, 40, 50];
 function calcularPromedio(array) {
     if (array.length === 0) {
         return "El array está vacío. No se puede calcular el promedio.";
     }
 
-    const suma = array.reduce((acumulador, elemento) => acumulador + elemento, 0);
-    const promedio = suma / array.length;
+    const sum = array.reduce((acumulator, elemet) => acumulator + elemet, 0);
+    const average = sum / array.length;
 
-    return  console.log("El promedio de los elementos es:", promedio);
+    return  console.log("El promedio de los elementos es:", average);
 }
-calcularPromedio(numeros);
+calcularPromedio(numbers);
