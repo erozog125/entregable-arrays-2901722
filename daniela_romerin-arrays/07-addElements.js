adaa262ba5167d6f01bb71b4b7df7e9926788382
unknown <SenaTarde@ARMAPRRQDFSD098.FormacionSena.RED>
2024-05-07 13:28:42 -0500

    Write a function that takes an array as an argument and returns a new array with the elements in reverse order. danniela romerin

---
+++ daniela_romerin-arrays/07-addElements.js
@@ -0,0 +1,7 @@
+function reverseArray(array) {
+    return array.slice().reverse();
+  }
+  
+  // Usage example
+  const originalArray = [3, 8, 2, 5, 1];
+  console.log(reverseArray(originalArray)); // This will return [1, 5, 2, 8, 3]
