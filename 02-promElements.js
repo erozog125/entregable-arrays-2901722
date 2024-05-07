d90a134a96440af1379c807d7d4128badf77b5a8
unknown <SenaTarde@ARMAPRRQDFSD105.FormacionSena.RED>
2024-05-07 13:27:05 -0500

    fead:Removed some duplicate files from the folder. Santaigo_Taborda-Arrays

--- 02-promElements.js
+++
@@ -1,7 +0,0 @@
-const numbers =[100, 300]
-
-function prom(numbers) {
-    return numbers.reduce ((a,b)=> a + b, 0) /numbers.length; 
-    
-}
-console.log(prom(numbers))
--- 03-removeDuplicates.js
+++
@@ -1,8 +0,0 @@
-const duplicate = ["papá", "mamá", "papá","santi","santi"];
-const noduplicated = eliminate(duplicate);
-
-function eliminate(array) {
-    return array.filter((valor, indice) => array.indexOf(valor) === indice);
-}
-
-console.log("Sin duplicar:", noduplicated);
