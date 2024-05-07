cb51d308c920eb6fbbf7c0dd4ca22d2ffd72d959
unknown <SenaTarde@ARMAPRCCIFOD593.FormacionSena.RED>
2024-05-07 15:15:31 -0500

    feat:Eliminar elementos duplicados. Santiago_Aguirre

--- Santiago_Aguirre-Arrays/03-duplicates.js
+++ Santiago_Aguirre-Arrays/03-duplicates.js
@@ -1,5 +1,5 @@
    function numbers(){
-    let array = [1,1,2,2,3,4,5,5];
+    let array = [1,2,2,3,4,5,5];
     let duplicates = new Set(array);
     array = Array.from(duplicates);
     return array
